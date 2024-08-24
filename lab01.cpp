8640d903ed06594d91ac329c5d6a4c770aa4f02c
Manuel-DeAbreu <skyland452@gmail.com>
2024-08-24 01:26:18 +0000

    Final Version

---
+++ lab01.cpp
@@ -0,0 +1,17 @@
+// Manuel De Abreu, Manuel-DeAbreu
+// A program to practice modifications and compilation
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello Solar System!" << std::endl;
+  std::cout << "Hello Galaxy!" << std::endl;
+  std::cout << "Hello Universe!" << std::endl;
+  std::cout << "Hello...?" << std::endl;
+
+  for(int i=1;i<6;i++){
+    std::cout << "This is a " << i << "!" << std::endl;
+  }
+  return 0;
+}
